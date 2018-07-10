21096962d5bdfe620bbc10977b7f4bf5af5ca08e
Tori Rojas <toriw22@gmail.com>
2018-07-10 13:28:37 -0700

    step 2 of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,43 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/main/about-me/about-me.component';
+import { PortfolioComponent } from './components/main/portfolio/portfolio.component';
+import { ContactComponent } from './components/main/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true },
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+const appRoutes: Routes = [
+
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  // { path: '',
+  //   redirectTo: '/about-me',
+  //   pathMatch: 'full'
+  // }
+];
+
+
 export class AppModule { }
