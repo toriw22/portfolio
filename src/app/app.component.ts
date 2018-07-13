8abb84052ac37b77a4b92282e59493a65ec133ea
Tori Rojas <toriw22@gmail.com>
2018-07-12 19:14:03 -0700

    fixed animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0deg)' })),
+      state('turnOut', style({ transform: 'rotate(-90deg) translateY(-114%)' })),
+      transition('default => turnOut', animate('800ms ease-out' )),
+      transition('turnOut => default', animate('800ms ease-out' )),
+    ]),
+
+  ]
 })
 export class AppComponent {
+  state: string = "default";
   title = 'app';
+
+  
+  rotate() {
+    this.state = (this.state === 'default' ? 'turnOut' : 'default' );
+  }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,36 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
-    trigger('rotatedState', [
-      state('default', style({ transform: 'rotate(0deg)' })),
-      state('turnOut', style({ transform: 'rotate(-54deg) translateX(-60%) translateY(-85%)' })),
-      transition('default => turnOut', animate('800ms ease-out' )),
-      transition('turnOut => default', animate('800ms ease-out' )),
-    ]),
-
-  ]
+  
 
 })
 export class HomeComponent implements OnInit {
 
-  state: string = "default";
+ 
   constructor() { }
 
   ngOnInit() {
   }
 
  
-
-  rotate() {
-    this.state = (this.state === 'default' ? 'turnOut' : 'default' );
-  }
 
 
 
