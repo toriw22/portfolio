25cd78fa51050bd737acf1491d1eed7784b3a31b
Tori Rojas <toriw22@gmail.com>
2018-07-23 14:13:44 -0700

    fixed animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
-import { trigger, state, query, style, animate, transition, group, animateChild } from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
     trigger('rotatedState', [
       state('default', style({ transform: 'rotate(0deg)' })),
       state('turnOut', style({ transform: 'rotate(-90deg) translateY(-114%)' })),
       transition('default => turnOut', animate('800ms ease-out' )),
       transition('turnOut => default', animate('800ms ease-out' )),
     ]),
-
   ]
 })
 export class AppComponent {
-
+  state: string = 'default';
   
   rotate() {
     this.state = (this.state === 'default' ? 'turnOut' : 'turnOut' );
